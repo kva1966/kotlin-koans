af540c9c1fdec35442b3cc19fd78eeb9e13f9a0a
Kamal Advani <kamal@namingcrisis.net>
2016-08-07 19:39:45 +1000

    Complete vi_generics koans.

--- src/vi_generics/_41_GenericFunctions.kt
+++ src/vi_generics/_41_GenericFunctions.kt
@@ -19,12 +19,20 @@ fun task41(): Nothing = TODO(
         }
 )
 
+fun <T, REC: Collection<T>, C: MutableCollection<T>> REC.partitionTo(out1: C, out2: C, predicate: (T) -> Boolean): Pair<C, C> {
+    val (c1, c2) = this.partition(predicate)
+    c1.toCollection(out1)
+    c2.toCollection(out2)
+    
+    return Pair(out1, out2)    
+}
+
 fun List<String>.partitionWordsAndLines(): Pair<List<String>, List<String>> {
-    task41()
-//    return partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
+//    task41()
+    return partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
 }
 
 fun Set<Char>.partitionLettersAndOtherSymbols(): Pair<Set<Char>, Set<Char>> {
-    task41()
-//    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z'}
+//    task41()
+    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z'}
 }
